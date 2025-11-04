0ff9ffb3e4957fc87940b312eb4e0e83de8ace0e
HeikyuDev <nahuelpedernera2005@gmail.com>
2025-11-04 16:33:52 -0300

    Se realizo el template con la navbar y el footer

---
+++ servisoft/src/main/java/com/gpp/servisoft/ServisoftApplication.java
@@ -0,0 +1,13 @@
+package com.gpp.servisoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServisoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServisoftApplication.class, args);
+	}
+
+}
---
+++ servisoft/src/test/java/com/gpp/servisoft/ServisoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gpp.servisoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServisoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
